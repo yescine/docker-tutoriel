4afe93854e6d66d9b773ae7b439c19d4873f4678
yescine <bouchoucha.yassine@gmail.com>
2021-11-18 21:04:27 +0100

    first optimization

--- command.sh
+++ command.sh
@@ -1,8 +1,11 @@
 # docker build
 docker build . #path
 # expose localPort:containerPort and image ids
-docker run -p 4000:3000 f5581dd12462b5fb3f229d7a91c14bfed9c412e18dd7698e38f8958a2db3d314
+docker run -p 80:3000 f5581dd12462b5fb3f229d7a91c14bfed9c412e18dd7698e38f8958a2db3d314
 
 # start and stop image by name
 docker ps
 docker stop <image_name>
+
+# docker hub
+docker run -it node
