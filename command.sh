3ebf9e3f663a0fd595db39505ace35e28be9e0e1
yescine <bouchoucha.yassine@gmail.com>
2021-11-19 19:53:32 +0100

    name and tags

--- command.sh
+++ command.sh
@@ -1,3 +1,7 @@
+# show container and images
+docker ps -a
+docker images
+docker images inspect <image id>
 # docker build
 docker build . #path
 # expose localPort:containerPort and image ids == build a container
@@ -11,6 +15,36 @@ docker stop <image_name>
 docker run -it node
 
 # start existing container
-docker start <container name>
-docker attach <container name>
+docker start -a <container name>
+docker attach <container name> # attach to a running container
 docker logs -f <container name>
+
+# attach an interactive shell
+docker run -i -t 1a4e3a4cfaeeca169
+
+# remove container
+docker rm <container name> <container name2> <container name3>
+docker rmi <image id> <image id2>
+docker image prune
+
+# copy file into container
+docker cp folder/. <container name>:/test
+
+###
+# best way to run
+docker build -t goals:v1.0.0 .
+docker run -p 80:3000 -d --rm --name <app new name> <image id>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
