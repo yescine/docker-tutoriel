bad675feb3f8c217091eb22390d93fc022c2109b
yescine <bouchoucha.yassine@gmail.com>
2021-11-19 13:46:06 +0100

    attach and detached

--- command.sh
+++ command.sh
@@ -1,6 +1,6 @@
 # docker build
 docker build . #path
-# expose localPort:containerPort and image ids
+# expose localPort:containerPort and image ids == build a container
 docker run -p 80:3000 f5581dd12462b5fb3f229d7a91c14bfed9c412e18dd7698e38f8958a2db3d314
 
 # start and stop image by name
@@ -9,3 +9,8 @@ docker stop <image_name>
 
 # docker hub
 docker run -it node
+
+# start existing container
+docker start <container name>
+docker attach <container name>
+docker logs -f <container name>
